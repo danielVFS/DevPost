ff107d237807a3190b1eefb833f42066797ac39b
danielVFS <danielv759153@gmail.com>
2020-12-04 11:08:55 -0300

    add: initial dependences

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
