6b6425141cd7887c32e44bf54c22ff3380c8ed61
danielVFS <danielv759153@gmail.com>
2020-12-04 17:44:17 -0300

    Login screen

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text} from 'react-native';
+
+import {
+  Container,
+  Title,
+  Input,
+  Button,
+  ButtonText,
+  SignUpButton,
+  SignUpButtonText,
+} from './styles';
 
 export default function Login() {
+  const [login, setLogin] = useState(true);
+
+  function toggleLogin() {
+    setLogin(!login);
+  }
+
+  if (login) {
+    return (
+      <Container>
+        <Title>
+          Dev<Text style={{color: '#E52246'}}>Post</Text>
+        </Title>
+
+        <Input placeholder="email@gmail.com" />
+        <Input placeholder="*******" secureTextEntry={true} />
+
+        <Button>
+          <ButtonText>Acessar</ButtonText>
+        </Button>
+
+        <SignUpButton onPress={toggleLogin}>
+          <SignUpButtonText>Criar uma conta</SignUpButtonText>
+        </SignUpButton>
+      </Container>
+    );
+  }
+
   return (
-    <View>
-      <Text>Login!</Text>
-    </View>
+    <Container>
+      <Title>
+        Dev<Text style={{color: '#E52246'}}>Post</Text>
+      </Title>
+
+      <Input placeholder="Seu Nome" />
+      <Input placeholder="email@gmail.com" />
+      <Input placeholder="*******" secureTextEntry={true} />
+
+      <Button>
+        <ButtonText>Cadastrar</ButtonText>
+      </Button>
+
+      <SignUpButton onPress={toggleLogin}>
+        <SignUpButtonText>Já tenho uma conta.</SignUpButtonText>
+      </SignUpButton>
+    </Container>
   );
 }
