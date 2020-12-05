28eedb9548115e86b33927a62d18e19db595e46c
danielVFS <danielv759153@gmail.com>
2020-12-05 09:32:36 -0300

    feat: AuthContext

--- App.js
+++ App.js
@@ -4,17 +4,21 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 
+import AuthProvider from './src/hooks/auth';
+
 import Routes from './src/routes';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar
-        backgroundColor="#36393F"
-        barStyle="light-content"
-        translucent={false}
-      />
-      <Routes />
+      <AuthProvider>
+        <StatusBar
+          backgroundColor="#36393F"
+          barStyle="light-content"
+          translucent={false}
+        />
+        <Routes />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
