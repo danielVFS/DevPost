67195ecb195af3c9cae7eb2071fb63b49536cae5
danielVFS <danielv759153@gmail.com>
2020-12-05 16:02:45 -0300

    feat: tabBar

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import Icon from 'react-native-vector-icons/Feather';
+
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import Search from '../pages/Search';
@@ -11,10 +13,43 @@ const Tab = createBottomTabNavigator();
 
 function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        showLabel: false,
+        style: {
+          backgroundColor: '#202225',
+          borderTopWidth: 0,
+        },
+        activeTintColor: '#FFF',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Icon name="home" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Icon name="search" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <Icon name="user" color={color} size={size} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
