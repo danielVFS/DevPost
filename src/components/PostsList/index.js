0b52e419ad9f43090d4f3187b4bf6679e204b7b4
danielVFS <danielv759153@gmail.com>
2020-12-07 16:12:52 -0300

    feat: fetch posts from firebase

---
+++ src/components/PostsList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Container, Text} from './styles';
+
+export default function PostsList() {
+  return (
+    <Container>
+      <Text>Flat</Text>
+    </Container>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import Icon from 'react-native-vector-icons/Feather';
+import React, {useState, useContext, useEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+import {View, ActivityIndicator} from 'react-native';
+
+import {AuthContext} from '../../hooks/auth';
+
+import Icon from 'react-native-vector-icons/Feather';
 
 import Header from '../../components/Header';
+import PostsList from '../../components/PostsList';
 
-import {Container, ButtonPost} from './styles';
+import {Container, ButtonPost, ListPosts} from './styles';
 
 export default function Home() {
   const navigation = useNavigation();
 
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const {user} = useContext(AuthContext);
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('posts')
+      .orderBy('created', 'desc')
+      .onSnapshot((snapshot) => {
+        const postList = [];
+
+        snapshot.forEach((doc) => {
+          postList.push({
+            ...doc.data(),
+            id: doc.id,
+          });
+        });
+
+        setPosts(postList);
+        setLoading(false);
+      });
+
+    return () => subscriber();
+  }, []);
+
   return (
     <Container>
       <Header />
+
+      {loading ? (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator size={50} color="#e52246" />
+        </View>
+      ) : (
+        <ListPosts data={posts} renderItem={({item}) => <PostsList />} />
+      )}
 
       <ButtonPost onPress={() => navigation.navigate('NewPost')}>
         <Icon name="edit-2" color="#FFF" size={25} />
